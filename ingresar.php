3d09e39048f1b8ae1d9e637226cb50666a747eaa
cderian <c.derian_11@ciencias.unam.mx>
2019-10-18 12:55:51 -0500

    Versión para probar en el servidor

--- ingresar.php
+++ ingresar.php
@@ -1,9 +1,8 @@
 <?php
-	//require_once('display_error.php');
-	$usObt=$_POST["usuario"];
-	$pasObt=$_POST["pass"];
-	
-	
+	$usObt=$_POST["username"];
+	$pasObt=$_POST["password"];
+
+
 	class ConexionBD{
 		private $user;
 		private $server;
@@ -11,7 +10,7 @@
 		private $dataBase;
 		private $port;
 		public $resultado;
-		
+
 		public function __construct($user,$server,$pass, $db, $port){
 			$this->user=$user;
 			$this->server=$server;
@@ -20,23 +19,22 @@
 			$this->port=$port;
 			$this->resultado =pg_connect("host=$this->server port=$this->port dbname=$this->dataBase user=$this->user password=$this->password") or die ("No se pudo conectar");
 		}
-    }
-    
+
+	}
 	class Usuario{
 		public $user;
 		public $pass;
-		
+
 		public function __construct($u,$p){
 			$this->user=$u;
 			$this->pass=$p;
 		}
-    }
-    
+	}
 	$ingresado = new Usuario($usObt, $pasObt);
-	$conectBD= new ConexionBD("RedApp","localhost","RedApp","RedApp","5432");
-    $dbconn = $conectBD->resultado;
-    
-	$result =pg_query($dbconn, "SELECT usuario, password FROM usuario WHERE usuario='$ingresado->user' AND password='".hash("sha256",$ingresado->pass)."'");
+	$conectBD= new ConexionBD("redapp","127.0.0.1","RedApp","redapp","5432");
+	$dbconn = $conectBD->resultado;
+    //$result =pg_query($dbconn, "SELECT usuario, password FROM Usuario WHERE usuario='$ingresado->user' AND password='".hash("sha256",$ingresado->pass)."'");
+    $result =pg_query($dbconn, "SELECT usuario, password FROM Usuario WHERE usuario='$ingresado->user' AND password='$ingresado->pass'");
 	if(!$result){
 		echo "Ocurrio un error.<br>";
 		exit;
@@ -45,7 +43,8 @@
 	if(empty($row)){
 		echo "<h1>Acceso denegado</h1>";
 	}else{
-		echo "Hola: ".$row['usuario']."</h1>";
+        //echo "Hola: ".$row['usuario']."</h1>";
+        header("Location:gato.html"); 
 	}
 	pg_close($dbconn);
 ?>
