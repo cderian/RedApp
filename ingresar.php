a95bd9dc4a4f89ba233f9f63addef54a16a6872d
Derian Estrada <36350572+cderian@users.noreply.github.com>
2019-11-24 12:17:34 -0600

    Cambiando autenticación de usuario

--- ingresar.php
+++ ingresar.php
@@ -1,50 +1,20 @@
 <?php
-	$usObt=$_POST["username"];
-	$pasObt=$_POST["password"];
+        $username = htmlspecialchars(strip_tags(trim($_POST["username"])), ENT_QUOTES, 'UTF-8');
+        $password = htmlspecialchars(strip_tags(trim($_POST["password"])), ENT_QUOTES, 'UTF-8');
 
+        $dbconn = pg_connect("host=127.0.0.1 port=5432 dbname=redapp user=redapp password=RedApp") or die ("No se pudo conectar");
+        $query = "SELECT usuario,password FROM Usuario WHERE username='$username' AND password=MD5('$password')";
+        $result = pg_query($dbconn, $query);
 
-	class ConexionBD{
-		private $user;
-		private $server;
-		private $password;
-		private $dataBase;
-		private $port;
-		public $resultado;
-
-		public function __construct($user,$server,$pass, $db, $port){
-			$this->user=$user;
-			$this->server=$server;
-			$this->password=$pass;
-			$this->dataBase=$db;
-			$this->port=$port;
-			$this->resultado =pg_connect("host=$this->server port=$this->port dbname=$this->dataBase user=$this->user password=$this->password") or die ("No se pudo conectar");
-		}
-
-	}
-	class Usuario{
-		public $user;
-		public $pass;
-
-		public function __construct($u,$p){
-			$this->user=$u;
-			$this->pass=$p;
-		}
-	}
-	$ingresado = new Usuario($usObt, $pasObt);
-	$conectBD= new ConexionBD("redapp","127.0.0.1","RedApp","redapp","5432");
-	$dbconn = $conectBD->resultado;
-    //$result =pg_query($dbconn, "SELECT usuario, password FROM Usuario WHERE usuario='$ingresado->user' AND password='".hash("sha256",$ingresado->pass)."'");
-    $result =pg_query($dbconn, "SELECT usuario, password FROM Usuario WHERE usuario='$ingresado->user' AND password='$ingresado->pass'");
-	if(!$result){
-		echo "Ocurrio un error.<br>";
-		exit;
-	}
-	$row= pg_fetch_assoc($result);
-	if(empty($row)){
-		echo "<h1>Acceso denegado</h1>";
-	}else{
-        //echo "Hola: ".$row['usuario']."</h1>";
-        header("Location:gato.html"); 
-	}
-	pg_close($dbconn);
+        if(!$result){
+                echo "Ocurrio un error.<br>";
+                exit;
+        }
+        $row= pg_fetch_assoc($result);
+        if(empty($row)){
+                echo "<h1>Acceso denegado</h1>";
+        }else{
+                header("Location:gato.html");
+        }
+        pg_close($dbconn);
 ?>
